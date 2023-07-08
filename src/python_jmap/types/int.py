ea4fbb1086d3ebfb6c07d748c0e71b1eef8560a7
Josh Finch <me@joshfinch.com>
2023-07-08 21:56:16 +0000

    Added specified Int and UnsignInt type-helpers

---
+++ src/python_jmap/types/int.py
@@ -0,0 +1,40 @@
+"""JMAP integer types.
+
+Spec: https://jmap.io/spec-core.html#the-int-and-unsignedint-data-types
+"""
+
+
+class Int(int):
+    """Integer in the range -2^53+1 to 2^53-1.
+
+    Example:
+    >>> x = Int(10)
+    >>> print(x)
+    10
+    >>> x = Int(2**53)  # Should raise ValueError
+    """
+
+    def __new__(cls, value: int) -> "Int":
+        """Creates a new instance of the Int class."""
+        if not (-(2**53) + 1 <= value <= 2**53 - 1):
+            raise ValueError(
+                "Value not within acceptable range (-2^53+1 <= value <= 2^53-1)"
+            )
+        return super().__new__(cls, value)
+
+
+class UnsignedInt(Int):
+    """Integer where the value MUST be in the range 0 to 2^53-1.
+
+    Example:
+    >>> x = UnsignedInt(10)
+    >>> print(x)
+    10
+    >>> x = UnsignedInt(-1)  # Should raise ValueError
+    """
+
+    def __new__(cls, value: int) -> "UnsignedInt":
+        """Creates a new instance of the UnsignedInt class."""
+        if not (0 <= value <= 2**53 - 1):
+            raise ValueError("Value not within acceptable range (0 <= value <= 2^53-1)")
+        return super().__new__(cls, value)
---
+++ tests/types/test_int.py
@@ -0,0 +1,46 @@
+"""Unit tests for JMAP integer types."""
+import pytest
+
+from python_jmap.types.int import Int
+from python_jmap.types.int import UnsignedInt
+
+
+def test_int_edge_cases() -> None:
+    """It should allow integers between -2^53+1 and 2^53-1 (inclusive)."""
+    assert Int(-(2**53) + 1) == -(2**53) + 1
+    assert Int(2**53 - 1) == 2**53 - 1
+    assert Int(0) == 0
+
+
+def test_int_invalid_values() -> None:
+    """It should raise ValueError for numbers outside this range."""
+    with pytest.raises(ValueError):
+        Int(-(2**53))
+    with pytest.raises(ValueError):
+        Int(2**53)
+
+
+def test_int_non_integer_input() -> None:
+    """It should raise ValueError for non-integer inputs."""
+    with pytest.raises(ValueError):
+        Int("test")  # Non-integer input
+
+
+def test_unsigned_int_edge_cases() -> None:
+    """It should allow integers between 0 and 2^53-1 (inclusive)."""
+    assert UnsignedInt(0) == 0
+    assert UnsignedInt(2**53 - 1) == 2**53 - 1
+
+
+def test_unsigned_int_invalid_values() -> None:
+    """It should raise ValueError for numbers outside this range."""
+    with pytest.raises(ValueError):
+        UnsignedInt(-1)
+    with pytest.raises(ValueError):
+        UnsignedInt(2**53)
+
+
+def test_unsigned_int_non_integer_input() -> None:
+    """It should raise ValueError for non-integer inputs."""
+    with pytest.raises(ValueError):
+        UnsignedInt("test")  # Non-integer input
