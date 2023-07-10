d9dd57f76bcc5aea7e810d32e79c596311a8d698
Josh Finch <me@joshfinch.com>
2023-07-10 21:04:24 +0000

    Fixed casing on root __all__

--- src/python_jmap/__init__.py
+++ src/python_jmap/__init__.py
@@ -3,6 +3,7 @@ from python_jmap.types import ID
 from python_jmap.types import Date
 from python_jmap.types import Int
 from python_jmap.types import UInt
+from python_jmap.types import UTCDate
 
 
-__all__ = ["Date", "ID", "Int", "UInt"]
+__all__ = ["ID", "Date", "UTCDate", "Int", "UInt"]
