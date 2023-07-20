7d4f53a19430f1d19cb41f241f4d5606bf831ed4
Josh Finch <me@joshfinch.com>
2023-07-20 12:31:01 +0000

    Deprecate Python 3.7 support

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
 
 
 PACKAGE = "python_jmap"
-PYTHON_VERSIONS = ["3.10", "3.11", "3.9", "3.8", "3.7"]
+PYTHON_VERSIONS = ["3.10", "3.11", "3.9", "3.8"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
